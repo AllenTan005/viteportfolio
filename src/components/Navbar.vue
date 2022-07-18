bd88f814c3a09a078607c25be02668e9b8d9ebc7
allentan005 <allentkm9705@gmail.com>
2022-07-18 17:36:25 +0800

    push company 5.30

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="fixed flex justify-between h-[10vh] p-3 w-full">
+    <div class=" flex justify-between items-center h-[10vh] p-3 w-full">
         <div class="w-[50%] pl-5">
             <img src="../assets/logo.svg" alt="" />
         </div>
         <div class="w-[50%] flex justify-end pr-5">
-            <button class=" w-[25%] h-auto bg-transparent
+            <button class=" w-[15%] h-auto py-3 bg-transparent
              rounded-md border-[#B5BBFF] border-2 text-xl text-[#B5BBFF] hover:bg-[#B5BBFF] hover:text-white ">contact
                 me</button>
         </div>
